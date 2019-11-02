785894261fa4c87e0ef33e8907b74ce1bb3dcabd
Jody Jones <jodybjones@yahoo.com>
2019-11-01 22:33:56 -0400

    need to figure out how to get the generate button to workgit status! Everything else should check out. going to include puntuations with number array since they are right on top of numbers anyway...keep them all in one simple slot. Work on making sure the generate option works in the morning.

--- style.js
+++ style.js
@@ -1,36 +1,46 @@
 
 var userInput = passwordChars;
-var lowerCaseLetters = [a-z];
-var upperCaseLetter = [A-Z];
-var digits = [0-9];
-var diffsymb = ["#","@","!","%","&","(",")","/"]
-var alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#A!%&()/"
-
-function makepwd(){
-document.getElementById("checkbox").addEventListener("click", function(){
-if (i = 0; i===lowerCaseLetters.length || i === upperCaseLetter || i === digits || i === diffsymb; i++)
-})
-
-}
+var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var digits = [0,1,2,3,4,5,6,7,8,9];
+var diffsymb = ["#","@","!","%","&","(",")","/"];
+smallLetters = document.getElementById(lowerCaseClick);
+bigLetters = document.getElementById(upperCaseClick);
+integers = document.getElementById(numbers);
 
 
+                    function validateCheckbox() {
+                    userPassword = "";
+                    passwordCharSet = "";
+                    
+                    if (smallLetters.checked) {
+                      passwordCharSet += lowerCaseLetters;
+                    }
+                    if (bigLetters.checked) {
+                      passwordCharSet += upperCaseLetter;
+                    }
+                    if (punctuationClick.checked) {
+                      passwordCharSet += punctuation;
+                    }
+                    if (numbersClick.checked) {
+                      passwordCharSet += numbers;
+                    }
+                    passwordlength = Number(lengthInput.value);
+                   
+                    for (let i = 0; i < plength; i++) {
+                      userPassword += passwordCharSet.charAt(
+                        Math.floor(Math.random() * passwordCharSet.length)
+                      );
+                      }
 
-
-
-function random_password_generate(max,min)
+                      function random_password_generate(max,min)
                 {
-                    var passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
+                    var passwordChars = [lowerCaseLetters,upperCaseLetter,digits,diffsymb];
                     var randPwLen = Math.floor(Math.random() * (max - min + 1)) + min;
                     var randPassword = Array(randPwLen).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
                     return randPassword;
                 }
                 document.getElementById("generatePassword").addEventListener("click", function(){
-                    random_password = random_password_generate(16,8);
+                    random_password = random_password_generate(128,8);
                     document.getElementById("randomPassword").value = random_password;
                 });
-
-
-
-
-
- }
