664ad3f6bc776efb502f373635a06fcc4b56bf6c
Jody Jones <jodybjones@yahoo.com>
2019-11-01 20:11:29 -0400

    currently updating pages. making all the proper text on html document while adding a little bit of style on css page. able to make the generate password button become clickable but no password is being generated just yet. need to make the button responsive to clicks and create a password. Need to also create another box for user input. After user inputs password....need to run a function that checks if the user is following the parameters. Same also goes for what the user clicks on as their password parameters. need to create functions and loops to test and validategit status

---
+++ style.js
@@ -0,0 +1,30 @@
+
+var userInput = passwordChars;
+var lowerCaseLetters = [a-z];
+var upperCaseLetter = [A-Z];
+var digits = [0-9];
+var diffsymb = ["#","@","!","%","&","(",")","/"]
+
+
+
+
+
+
+
+function random_password_generate(max,min)
+                {
+                    var passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
+                    var randPwLen = Math.floor(Math.random() * (max - min + 1)) + min;
+                    var randPassword = Array(randPwLen).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+                    return randPassword;
+                }
+                document.getElementById("generatePassword").addEventListener("click", function(){
+                    random_password = random_password_generate(16,8);
+                    document.getElementById("randomPassword").value = random_password;
+                });
+
+
+
+
+
+ }
